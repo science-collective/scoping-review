b05123af09e53385d547973104584eb9afafc993
Luke W. Johnston <lwjohnst@gmail.com>
2022-09-02 16:16:08 +0200

    add search terms as an R variable

---
+++ R/search-terms.R
@@ -0,0 +1,4 @@
+search_terms <- list(
+    general = "(open) AND (science OR research) AND (collaborat* OR team OR cooperat*) AND (technolog* OR tool OR framework OR guideline OR principles OR practices OR systems OR resources)",
+    pubmed = "(open[Title]) AND (science OR research) AND (collaborat* OR team OR cooperat*) AND (technolog* OR tool OR framework OR guideline OR principles OR practices OR systems OR resources) AND (y_5[Filter])"
+)
