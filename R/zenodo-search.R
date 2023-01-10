c736bc77e6c47a57d86441b4a86b9da81b1de283
Luke W. Johnston <lwjohnst@gmail.com>
2023-01-10 13:54:55 +0100

    styled file

--- R/zenodo-search.R
+++ R/zenodo-search.R
@@ -14,34 +14,33 @@ source(here("R/search-terms.R"))
 # Create Zenodo-specific search term --------------------------------------
 
 # For some reason Zenodo doesn't accept this publication_date query in the API
-five_years_ago <- glue("{today()-years(5)}")
 five_years_to_today <- glue("publication_date:[{five_years_ago} TO {today()}]")
 
 # Zenodo specific limiters
 limiters <- "
-    (access_right:open) AND
-    (
-        resource_type.type:publication
-        resource_type.type:poster
-        resource_type.type:presentation
-        resource_type.type:lesson
-        resource_type.type:other
-    )
+  (access_right:open) AND
+  (
+    resource_type.type:publication
+    resource_type.type:poster
+    resource_type.type:presentation
+    resource_type.type:lesson
+    resource_type.type:other
+  )
 "
 
 # Structured search term to fit Zenodo requirements
 search_term <- glue("{search_terms$zenodo} AND {limiters}") %>%
-    str_remove_all("\\n+") %>%
-    str_squish() %>%
-    str_remove_all("OR ")
+  str_remove_all("\\n+") %>%
+  str_squish() %>%
+  str_remove_all("OR ")
 
 # Connect to Zenodo and run search ----------------------------------------
 
 # Requires token (PAT) from Zenodo to work
 zenodo <- ZenodoManager$new(
-    url = "https://zenodo.org/api",
-    logger = "DEBUG",
-    token = askpass::askpass()
+  url = "https://zenodo.org/api",
+  logger = "DEBUG",
+  token = askpass::askpass()
 )
 
 # zenodo$getCommunities()
@@ -56,30 +55,30 @@ length(zenodo_records)
 # Process extracted Zenodo records ----------------------------------------
 
 extract_relevant_data <- function(record_list) {
-    creators <- record_list$metadata$creators %>%
-        map_chr( ~ .x$name) %>%
-        str_flatten(" and ") %>%
-        str_trim()
+  creators <- record_list$metadata$creators %>%
+    map_chr(~ .x$name) %>%
+    str_flatten(" and ") %>%
+    str_trim()
 
-    keywords <- record_list$metadata$keywords %>%
-        str_flatten("; ") %>%
-        str_trim()
+  keywords <- record_list$metadata$keywords %>%
+    str_flatten("; ") %>%
+    str_trim()
 
-    list(
-        creators = creators,
-        title = record_list$metadata$title,
-        description = record_list$metadata$description,
-        date = record_list$metadata$publication_date,
-        type = record_list$metadata$upload_type,
-        keywords = keywords,
-        doi = record_list$links$doi
-    )
+  list(
+    creators = creators,
+    title = record_list$metadata$title,
+    description = record_list$metadata$description,
+    date = record_list$metadata$publication_date,
+    type = record_list$metadata$upload_type,
+    keywords = keywords,
+    doi = record_list$links$doi
+  )
 }
 
 # Extract necessary data and keep only last five years
 zenodo_records_processed <- zenodo_records %>%
-    map(extract_relevant_data) %>%
-    keep(~ ymd(as_date(.x$date)) >= five_years_ago)
+  map(extract_relevant_data) %>%
+  keep(~ ymd(as_date(.x$date)) >= five_years_ago)
 
 # There's less entries from the original
 length(zenodo_records_processed)
