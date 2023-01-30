08ad6243c498a272b70d4e7a5a69afd90499b035
Luke W. Johnston <lwjohnst@gmail.com>
2023-01-30 20:39:53 +0100

    convert to a function

--- R/search-terms.R
+++ R/search-terms.R
@@ -1,7 +1,17 @@
-search_terms <- list(
+#' Engine-specific search terms.
+#'
+#' @param engine The "engine" to use for the search terms, like PubMed or Zenodo.
+#'
+#' @return A character vector.
+#'
+#' @examples
+#' search_terms("pubmed")
+search_terms <- function(engine) {
+  switch(engine,
     general = "(open) AND (science OR research) AND (collaborat* OR team OR cooperat*) AND (technolog* OR tool OR framework OR guideline OR principles OR practices OR systems OR resources)",
     pubmed = "(open[Title]) AND (science OR research) AND (collaborat* OR team OR cooperat*) AND (technolog* OR tool OR framework OR guideline OR principles OR practices OR systems OR resources) AND (y_5[Filter])",
     zenodo = "(title:open) AND (science OR research) AND (collaborat* OR team OR cooperat*) AND (technolog* OR tool OR framework OR guideline OR principles OR practices OR systems OR resources)",
     wos = "(TI=(open)) AND ((ALL=(science)) OR ALL=(research)) AND (((ALL=(collaborat*)) OR ALL=(team)) OR ALL=(cooperat*)) AND ((((((((ALL=(technolog*)) OR ALL=(tool)) OR ALL=(framework)) OR ALL=(guideline)) OR ALL=(principles)) OR ALL=(practices)) OR ALL=(systems)) OR ALL=(resources))",
     medrxiv = "open collaboration"
-)
+  )
+}
