881e58da05d773651b32ebcec7fe4804a4bb1229
Luke W. Johnston <lwjohnst@gmail.com>
2023-04-30 18:03:18 +0200

    start reviewing and refactoring pubmed data

--- R/pubmed-search.R
+++ R/pubmed-search.R
@@ -1,66 +1,77 @@
 # Pubmed search using easypubmed
 
-#Link: https://cran.r-project.org/web/packages/easyPubMed/vignettes/getting_started_with_easyPubMed.html
-
-# Load libraries ---------------------------------------------------------------
-
 library(easyPubMed)
 library(tidyverse)
 library(here)
 library(purrr)
 library(readr)
+library(xml2)
 source(here("R/search-terms.R"))
-
-# Search last updated ----------------------------------------------------------
-
-Sys.Date() #2022-11-03
-
-# Search pubmed using the easyPubMed package -----------------------------------
 
 ## making a list with the search terms
 
-## search and download pubmed records
-pubmed_search <-
-    batch_pubmed_download(
-        pubmed_query_string = search_terms("pubmed"),
-        dest_file_prefix = "open_collab_",
-        encoding = "UTF-8"
-    )
+pubmed_get_records <- function(search_terms) {
+  article_ids <- easyPubMed::get_pubmed_ids(search_terms)
+  articles <- easyPubMed::fetch_pubmed_data(article_ids,
+    retmax = as.integer(article_ids$Count)
+  )
 
-# Extracting info and combining into single dataframe --------------------------
-
-## make a function that convert each XML file to a dataset
-make_df_from_pubmed <- function(dataset) {
-    new_pubmed_df <- easyPubMed::table_articles_byAuth(
-        pubmed_data = dataset,
-        included_authors = "first",
-        encoding = "UTF-8"
-    )
-    new_pubmed_df <- tibble::as_tibble(new_pubmed_df)
-    return(new_pubmed_df)
+  articles %>%
+    xml2::read_xml() %>%
+    xml2::xml_find_all(".//Article") %>%
+    as_list()
 }
 
-## use map to single dataframe of all the batches
-# !!note: this takes a while!!#
-open_collaboration_pubmed_df <-
-    map_dfr(pubmed_search, make_df_from_pubmed, .id = NULL)
+pubmed_extract_relevant_data <- function(record_list) {
+  metadata <- record_list %>%
+    purrr::keep_at(c("ArticleTitle", "ArticleDate", "ELocationID"))
+  date <- paste(
+    metadata$ArticleDate$Year,
+    metadata$ArticleDate$Month,
+    metadata$ArticleDate$Day,
+    sep = "-"
+  )
 
-# Cleaning dataset -------------------------------------------------------------
+  date <- ifelse(!rlang::is_empty(date), date, NA_character_)
 
-pubmed_df <- open_collaboration_pubmed_df %>%
-    select(lastname, year, title, abstract, jabbrv, email, pmid) %>%
-    mutate(database = "pubmed")
+  list(
+    doi = unlist(metadata$ELocationID),
+    date = date,
+    title = unlist(metadata$ArticleTitle)
+  )
+}
+
+pubmed_retrieve_records <- function(search_terms) {
+  browser()
+  pubmed_records_processed <- search_terms %>%
+    pubmed_get_records() %>%
+    purrr::map(pubmed_extract_relevant_data) %>%
+    purrr::keep(~ {
+      !is.na(.x$date) |
+        lubridate::ymd(lubridate::as_date(.x$date)) >= five_years_ago()
+    })
+
+  number_articles <- easyPubMed::get_pubmed_ids(search_terms)$Count
+
+  cli::cli_inform(c("Records from PubMed",
+    "i" = "{number_articles} records were retrieved.",
+    "i" = "{length(pubmed_records_processed)} records are within 5 years."
+  ))
+
+  pubmed_records_processed
+}
+
+# Extracting info and combining into single dataframe --------------------------
 
 # Count number of papers -------------------------------------------------------
 
 # 756 papers identified when copying the search on pubmed
 
-n_papers_web <- 756
-n_papers_r <-  as.numeric(nrow(pubmed_df))
+# n_papers_web <- 756
+# n_papers_r <- as.numeric(nrow(pubmed_df))
+#
+# search_the_same <- n_papers_web == n_papers_r
+# search_the_same
 
-search_the_same <- n_papers_web == n_papers_r
-search_the_same
-
-# Save dataset -----------------------------------------------------------------
-
-readr::write_csv(pubmed_df, here("data", "pubmed-search.csv.gz"))
+search_terms("pubmed") %>%
+  pubmed_retrieve_records()
