770f48595dd5ee8a7c0b9f4e6e95e1ba12d3f39e
MarioGuCBMR <64911610+MarioGuCBMR@users.noreply.github.com>
2023-05-12 15:20:24 +0200

    Changes to medrxiv

--- R/search-terms.R
+++ R/search-terms.R
@@ -14,7 +14,7 @@ search_terms <- function(engine) {
     wos = "(TI=(open)) AND ((ALL=(science)) OR ALL=(research)) AND (((ALL=(collaborat*)) OR ALL=(team)) OR ALL=(cooperat*)) AND ((((((((ALL=(technolog*)) OR ALL=(tool)) OR ALL=(framework)) OR ALL=(guideline)) OR ALL=(principles)) OR ALL=(practices)) OR ALL=(systems)) OR ALL=(resources))",
     # For some reason, arxiv doesn't like the * wildcard... #Daniel: okay not to include. We will just state why here.
     arxiv = '(ti:open) AND (all:science OR all:research) OR (all:collaborate OR all:collaborating OR all:collaboration OR all:team OR all:cooperate OR all:cooperating OR all:cooperation) AND (all:technology OR all:technologies OR all:tool OR all:framework OR all:guideline OR all:principles OR all:practices OR all:systems OR all:resources)'
-    #Trying code for arxiv also for medrxiv. Assuming they have the same engine
+    #Changing according to the new tutorial: https://docs.ropensci.org/medrxivr/articles/medrxivr.html
     medrxiv = list(c("open"), c("science", "research"), c("collaborate","collaborating", "collaboration", "team", "cooperate", "cooperating"), c("technology", "technologies", "tool","framework", "guideline", "principles", "practices", "systems","resources"))
   )
 }
