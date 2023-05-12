ca658c5d1124e5ebc2b9a2f3f052d3122ec71725
Luke W. Johnston <lwjohnst@gmail.com>
2023-05-12 12:51:50 -0400

    small edits and comments

--- R/search-terms.R
+++ R/search-terms.R
@@ -11,11 +11,12 @@ search_terms <- function(engine) {
   switch(engine,
     general = "(open) AND (science OR research) AND (collaborat* OR team OR cooperat*) AND (technolog* OR tool OR framework OR guideline OR principles OR practices OR systems OR resources)",
     pubmed = "(open[Title]) AND (science OR research) AND (collaborate OR collaboration OR collaborating OR team OR cooperate OR cooperation OR cooperating) AND (technology OR technologies OR tool OR framework OR guideline OR principles OR practices OR systems OR resources) AND (y_5[Filter])",
+    # Doesn't like the wildcard...
     zenodo = "(title:open) AND (science OR research) AND (collaborate OR collaboration OR collaborating OR team OR cooperate OR cooperation OR cooperating) AND (technology OR technologies OR tool OR framework OR guideline OR principles OR practices OR systems OR resources)",
     wos = "(TI=(open)) AND ((ALL=(science)) OR ALL=(research)) AND (((ALL=(collaborat*)) OR ALL=(team)) OR ALL=(cooperat*)) AND ((((((((ALL=(technolog*)) OR ALL=(tool)) OR ALL=(framework)) OR ALL=(guideline)) OR ALL=(principles)) OR ALL=(practices)) OR ALL=(systems)) OR ALL=(resources))",
-    # For some reason, arxiv doesn't like the * wildcard...
+    # For some reason, arXiv doesn't like the * wildcard...
     arxiv = 'ti:"open" AND (research OR science) AND (collaborate OR collaborating OR collaboration OR team OR cooperate OR cooperating OR cooperation) AND (technology OR technologies OR tool OR framework OR guideline OR principles OR practices OR systems OR resources)',
-    #Trying code for arxiv also for medrxiv. Assuming they have the same engine
+    # Trying code for arxiv also for medrxiv. Assuming they have the same engine
     medrxiv ='ti:"open" AND (research OR science) AND (collaborate OR collaborating OR collaboration OR team OR cooperate OR cooperating OR cooperation) AND (technology OR technologies OR tool OR framework OR guideline OR principles OR practices OR systems OR resources)'
   )
 }
