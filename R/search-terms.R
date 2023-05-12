cabd21ac8f43759032de8dd9394199d3621c1fcc
MarioGuCBMR <64911610+MarioGuCBMR@users.noreply.github.com>
2023-05-12 09:55:25 +0200

    Updated arxiv search tesms

--- R/search-terms.R
+++ R/search-terms.R
@@ -13,7 +13,7 @@ search_terms <- function(engine) {
     zenodo = "(title:open) AND (science OR research) AND (collaborat* OR team OR cooperat*) AND (technolog* OR tool OR framework OR guideline OR principles OR practices OR systems OR resources)",
     wos = "(TI=(open)) AND ((ALL=(science)) OR ALL=(research)) AND (((ALL=(collaborat*)) OR ALL=(team)) OR ALL=(cooperat*)) AND ((((((((ALL=(technolog*)) OR ALL=(tool)) OR ALL=(framework)) OR ALL=(guideline)) OR ALL=(principles)) OR ALL=(practices)) OR ALL=(systems)) OR ALL=(resources))",
     # For some reason, arxiv doesn't like the * wildcard... #Daniel: okay not to include. We will just state why here.
-    arxiv = 'ti:"open" AND (research OR science) AND (collaborate OR collaborating OR collaboration OR team OR cooperate OR cooperating OR cooperation) AND (technology OR technologies OR tool OR framework OR guideline OR principles OR practices OR systems OR resources)',
+    arxiv = '(ti:open) AND (all:science OR all:research) OR (all:collaborate OR all:collaborating OR all:collaboration OR all:team OR all:cooperate OR all:cooperating OR all:cooperation) AND (all:technology OR all:technologies OR all:tool OR all:framework OR all:guideline OR all:principles OR all:practices OR all:systems OR all:resources)'
     #Trying code for arxiv also for medrxiv. Assuming they have the same engine
     medrxiv ='ti:"open" AND (research OR science) AND (collaborate OR collaborating OR collaboration OR team OR cooperate OR cooperating OR cooperation) AND (technology OR technologies OR tool OR framework OR guideline OR principles OR practices OR systems OR resources)'
   )
