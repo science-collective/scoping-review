577c873c22743c93fd1cd09f2869e7a310729723
Luke W. Johnston <lwjohnst@gmail.com>
2023-10-06 17:08:43 +0200

    add more exclusion criteria based on quick reviews of the refs

--- R/exclusions.R
+++ R/exclusions.R
@@ -5,20 +5,30 @@
 #' @return A tibble.
 #'
 #' @examples
-#' readr::read_csv(here::here("data-raw/zenodo.csv")) |> exclude_from_title() |> View()
+#' readr::read_csv(here::here("data-raw/pubmed.csv")) |> exclude_from_title() |> View()
 exclude_from_title <- function(data) {
 # TODO: Use ggcohort to track what is removed?
   data |>
-    # Items related to healthcare or surgery
-    exclude(title, "open( |-)label|surgery|surgical|angle glaucoma|fracture|abdomen|wound") |>
+    # Items related to surgery
+    exclude(title, "hysterectomy|surgery|surgical|angle glaucoma|fracture|abdomen|wound|colectomy|vein|resection|gastrectomy|urethroplasty") |>
+    # Items related to healthcare or health study design
+    exclude(title, "open( |-)(cohort|label|trial|radical|radiology|acute|study|weighing|registry)|rehab|neuroscience|rt-qpcr|randomi[zs]ed|neuronal|neurons") |>
     # Items related to government, politics, or social
-    exclude(title, "open( |-)(government|governance|dialogue|letter|spaces|call|people|universit(y|ies))") |>
+    exclude(title, "open( |-)(government|governance|dialog(ue)?|letter|spaces|call|people|universit(y|ies)|stewardship)") |>
     # Items related to electronics or tech
-    exclude(title, "open( |-)(circuit|shell|backend|set|iot)") |>
+    exclude(title, "open( |-)(electronics|circuit|shell|backend|set|iot|user|hollow|rdf)|hardware|blockchain|resonator|nanoparticle|robotic") |>
+    # Items related to physics or engineering
+    exclude(title, "resonator|nanoparticle|robotic|x-ray|open( |-)(universe|systems)|nanofluidic") |>
+    # Items related to specific software
+    exclude(title, "matlab|pathofusion|mrqy|imagej|opsef|open-tes") |>
     # Items related to dissemination
-    exclude(title, "open( |-)access|publishing|communication") |>
+    exclude(title, "open( |-)(access|review(er)?)|publishing|communication") |>
+    # Items related to education
+    exclude(title, "open( |-)(education(al)?|seminar|learning)|mooc|massive open online") |>
+    # Items related to industry/business
+    exclude(title, "open( |-)(leader(ship)?|marketplace|innovation|pit|biomass|fermentation|hydroponic)") |>
     # Items related to misc
-    exclude(title, "open( |-)(end(ed)?|issues|water|air|education(al)?)")
+    exclude(title, "open( |-)(end(ed)?|issues|synthesis|water|air|leader(ship)?|domain|platform|monitoring|tennis|questions|charm|games|competition)|hunter")
 }
 
 exclude <- function(data, variable, criteria) {
