62fc09c1fe0cff58c7211566db6295989b56bf78
Daniel Ibsen <dbi@ph.au.dk>
2024-01-30 14:29:49 +0100

    updated exclusions for search terms

--- R/exclusions.R
+++ R/exclusions.R
@@ -10,15 +10,17 @@ exclude_from_title <- function(data) {
 # TODO: Use ggcohort to track what is removed?
   data |>
     # Items related to surgery
-    exclude(title, "surgery|surgical|angle glaucoma|fracture|abdomen|abdominal|wound|vein|resection|urethroplasty|ectomy|inguinal|aortic") |>
+    exclude(title, "surgery|surgical|angle glaucoma|fracture|abdomen|abdominal|wound|vein|resection|urethroplasty|ectomy|inguinal|aortic|rhinoplasty|lumbar") |>
     # Items related to study designs
     exclude(title, "open( |-|, )(web-based)") |>
     # Items related to healthcare or health study design
-    exclude(title, "open( |-|, )(covid|defecation|comparative|cohort|pilot|double|label|trial|radical|radiology|acute|study|weighing|registry|care|selective|bay|chromatin|bite|repair|single-arm|wedge)|rehab|neuroscience|rt-qpcr|randomi[zs]ed|neuronal|neurons") |>
+    exclude(title, "open( |-|, )(covid|defecation|comparative|cohort|pilot|double|label|trial|radical|radiology|acute|study|weighing|registry|care|selective|bay|chromatin|bite|repair|single-arm|wedge)|rehab|neuroscience|rt-qpcr|randomi[zs]ed|neuronal|neurons|technique|heart|transforaminal") |>
     # Items related to biology
-    exclude(title, "open( |-|, )(microfluidic|membrane|pan-)") |>
+    exclude(title, "open( |-|, )(microfluidic|membrane|pan-|texture)") |>
+    exclude(title, "faecal|faeces") |>
     # Items related to government, politics, or social
-    exclude(title, "open( |-|, )(government|governance|dialog(ue)?|letter|spaces|call|people|universit(y|ies)|stewardship|space)") |>
+    exclude(title, "open( |-|, )(government|governance|dialog(ue)?|letter|spaces|call|people|universit(y|ies)|stewardship|space|labour|employment)") |>
+    exclude(title, "kindergarten") |>
     # Items related to electronics or tech
     exclude(title, "open( |-|, )(electronics|circuit|shell|backend|set|iot|user|hollow|rdf)|hardware|blockchain|resonator|nanoparticle|robotic") |>
     # Items related to physics or engineering
@@ -35,9 +37,9 @@ exclude_from_title <- function(data) {
     exclude(title, "open( |-|, )(leader(ship)?|marketplace|innovation|pit|economy|loop)") |>
     exclude(title, "trade openness") |>
     # Items related to ecology/environment
-    exclude(title, "open( |-|, )(water|air|coastal|metal|forecast(ing)?|field|pollinated|hydroponic|fermentation|biomass|mapping|landscape)|hunter") |>
+    exclude(title, "open( |-|, )(water|air|coastal|metal|forecast(ing)?|field|pollinated|hydroponic|fermentation|biomass|mapping|landscape)|hunter|burning|land|green") |>
     # Items related to sports, games
-    exclude(title, "open( |-|, )(tennis|games|competition|play)") |>
+    exclude(title, "open( |-|, )(tennis|games|competition|play|US)") |>
     # Items related to misc
     exclude(title, "open( |-|, )(end(ed)?|issues|synthesis|invitation|domain|platform|monitoring|questions|charm|porous|volume|case|top|disclosure|network|up|to |forensic|challenges?|days?|symposium|plan|house|model|multiscale)") |>
     # Random items that probably shouldn't be in the database
